dc04cdc532cfc13b6b8f1110f9f6840affbe58d4
JCASAltamirano <jaltamiranos@uea.edu.ec>
2026-08-30 11:58:28 -0500

    Agregamos Proyecto UEA

--- Matriz5x5.java
+++ Matriz5x5.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 public class Matriz5x5 {
     public static void main(String[] args) {
-        // Objeto Scanner para permitir el ingreso de datos por consola
         Scanner sc = new Scanner(System.in);
         int[][] matriz = new int[5][5];
-        System.out.println("=== INGRESE LOS DATOS A LA MATRIZ 5x5 ===");
+        System.out.println("=== INGRESO DE DATOS (MATRIZ 5x5) ===");
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print("Ingrese el valor numérico para la posición [" + i + "][" + j + "]: ");
